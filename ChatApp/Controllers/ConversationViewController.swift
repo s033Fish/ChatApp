645ee4ca1c2b1f91becade0532131c2bcda516a6
Spencer Fisher <s12105fish@outlook.com>
2021-12-28 19:06:44 -0500

    First push

---
+++ ChatApp/Controllers/ConversationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Spencer Fisher on 12/26/21.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+    }
+    
+    override func viewDidAppear( _ animated: Bool) {
+        super.viewDidAppear(animated)
+   
+
+        validateAuth()
+    }
+
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
+        
+}
+
---
+++ ChatApp/Resources/DatabaseManager.swift
@@ -0,0 +1,49 @@
+//
+//  DatabaseManager.swift
+//  ChatApp
+//
+//  Created by Spencer Fisher on 12/28/21.
+//
+
+import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    
+    static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+}
+
+// MARK: - Account Management
+
+extension DatabaseManager {
+    
+    public func userExists(with email: String,
+                           completion: @escaping ((Bool) -> Void)){
+        
+        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+            guard snapshot.value as? String != nil else {
+                completion(false)
+                return
+            }
+            
+            completion(true)
+        })
+    }
+    
+    /// Inserts new user to database
+    public func insertUser(with user: ChatAppUser) {
+        database.child(user.emailAddress).setValue([
+            "first_name": user.firstName,
+            "last_name": user.lastName
+        ])
+    }
+}
+
+struct ChatAppUser {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+    //let profilePictureUrl: String
+}
