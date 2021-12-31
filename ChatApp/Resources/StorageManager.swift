9a4f08d6ae9353fd4e1f100a9605453ceac53702
Spencer Fisher <s12105fish@outlook.com>
2021-12-31 16:30:59 -0500

    add profile pictures

---
+++ ChatApp/Resources/StorageManager.swift
@@ -0,0 +1,50 @@
+//
+//  StorageManager.swift
+//  ChatApp
+//
+//  Created by Spencer Fisher on 12/30/21.
+//
+
+import Foundation
+import FirebaseStorage
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    private let storage = Storage.storage().reference()
+    
+    /*
+     /images/23fisspe-hawken-edu_profile_picture.png
+     */
+    
+    public typealias UploadPictureCompletion = (Result<String, Error>)
+    
+    /// Uploads picture to firebase storage and returns completion with url string to download
+    public func uploadProfilePicture(with data: Data, fileName: String, completion: @escaping (UploadPictureCompletion) -> Void) {
+        storage.child("images/\(fileName)").putData(data, metadata: nil, completion: { metadata, error in
+            guard error == nil else {
+                print("Failed to upload data to firebase for picture")
+                completion(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            
+            self.storage.child("images/\(fileName)").downloadURL(completion: { url, error in
+                guard let url = url else {
+                    print("Failed to get download url")
+                    completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                    return
+                }
+                
+                let urlString = url.absoluteString
+                print("download url returned: \(urlString)")
+                completion(.success(urlString))
+            })
+        })
+    }
+    
+    public enum StorageErrors: Error {
+        case failedToUpload
+        case failedToGetDownloadUrl
+    }
+}
--- Podfile
+++ Podfile
@@ -7,6 +7,7 @@ target 'ChatApp' do
 pod 'Firebase/Core'
 pod 'Firebase/Auth'
 pod 'Firebase/Database'
+pod 'Firebase/Storage'
 
 # 'Google Sign In'
 pod 'GoogleSignIn', '5.0.0'
